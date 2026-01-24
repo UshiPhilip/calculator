b3cf8dc592e388e5afb996266817cf893ab7cb2e
Ushi Philip <ushi7670741@gmail.com>
2026-01-24 20:17:52 +0200

    Connecting operations to main

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Welcome to our calculator!")
-print("====================")
+from operations import add, subtract
+
+print("============================")
+print(" Welcome to our calculator!")
+print("============================")
+
+result = add(3,5)
+print(result)
+
+result = subtract(5,3)
+print(result)
