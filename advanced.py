48a961f0591ed04af8364efdf3731ba69815bc05
Ushi Philip <ushi7670741@gmail.com>
2026-01-24 20:56:32 +0200

    Added advanced functions

---
+++ advanced.py
@@ -0,0 +1,19 @@
+def power(base, exponent):
+    """
+    Power by
+    """
+    return base ** exponent
+
+def square_root(n):
+    """
+    Square root
+    """
+    if n < 0:
+        return "Error: No root for a negative number!"
+    return n ** 0.5
+
+def modulo(a, b):
+    """
+    Remainder from division
+    """
+    return a % b
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from operations import add, subtract, multiply, divide
+from advanced import power, square_root, modulo
 
 print("============================")
 print(" Welcome to our calculator!")
 print("============================")
+
+print("\nBasic actions:")
 
 result = add(3,5)
 print(f"3 + 5 = {result}")
@@ -15,3 +18,14 @@ print(f"3 * 5 = {result}")
 
 result = divide(5,3)
 print(f"5 / 3 = {result}")
+
+print("\nAdvance actions:")
+
+result = power(2,3)
+print(f"2 ** 3 = {result}")
+
+result = square_root(4)
+print(f"4 ** 0.5 = {result}")
+
+result = modulo(5,2)
+print(f"5 % 2 = {result}")
