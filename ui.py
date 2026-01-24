4ca23eea5ea9c1157908b8f99cc026439247fc5b
Ushi Philip <ushi7670741@gmail.com>
2026-01-24 21:04:53 +0200

    Added a user interface

---
+++ ui.py
@@ -0,0 +1,23 @@
+def get_number(prompt):
+    """
+    Catches a number from the user
+    """
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Error: Please enter a number!")
+
+def show_menu():
+    """
+    Shows the menu
+    """
+    print("\n=== MENU ===")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Power")
+    print("6. Root")
+    print("0. Exit")
+    return input("Chose an action: ")
