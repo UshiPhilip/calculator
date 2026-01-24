bfa609a61745d76542fdca1443d8e25bd7c3100d
Ushi Philip <ushi7670741@gmail.com>
2026-01-24 20:23:09 +0200

    Added multi & divide fun

--- operations.py
+++ operations.py
@@ -5,3 +5,17 @@ def add(a, b):
 def subtract(a, b):
     """Substructure two numbers"""
     return a - b
+
+def multiply(a, b):
+    """
+    Multiply two numbers
+    """
+    return a * b
+
+def divide(a, b):
+    """
+    Dividing two numbers
+    """
+    if b == 0:
+        return "Error: you can NOT dividing by zero!"
+    return a / b
