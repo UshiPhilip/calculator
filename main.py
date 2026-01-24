ea25a386bb6b1c47ac59d3dca441361bdb727018
Ushi Philip <ushi7670741@gmail.com>
2026-01-24 20:43:36 +0200

    Update all functions to the main file

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from operations import add, subtract
+from operations import add, subtract, multiply, divide
 
 print("============================")
 print(" Welcome to our calculator!")
 print("============================")
 
 result = add(3,5)
-print(result)
+print(f"3 + 5 = {result}")
 
 result = subtract(5,3)
-print(result)
+print(f"5 - 3 = {result}")
+
+result = multiply(3,5)
+print(f"3 * 5 = {result}")
+
+result = divide(5,3)
+print(f"5 / 3 = {result}")
