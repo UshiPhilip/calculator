a86507b84e146b38d9c56a739e2f78355ef33a6d
Ushi Philip <ushi7670741@gmail.com>
2026-01-25 08:59:22 +0200

    Added factorial to advanced file

--- advanced.py
+++ advanced.py
@@ -7,13 +7,13 @@ Including advanced calculations: Power By, Square Root, Modulo
 
 def power(base, exponent):
     """
-    Power by
+    Power by.
     """
     return base ** exponent
 
 def square_root(n):
     """
-    Square root
+    Square root.
     """
     if n < 0:
         return "Error: No root for a negative number!"
@@ -21,6 +21,17 @@ def square_root(n):
 
 def modulo(a, b):
     """
-    Remainder from division
+    Remainder from division.
     """
     return a % b
+
+def factorial(n):
+    """
+    Calculate a factorial for a number.
+    """
+    if n < 0:
+        return "Error: There is no factorial to a negative number!"
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
