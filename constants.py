89e4be5fb7a3981bff1b32b82fcfbffdad152815
Ushi Philip <ushi7670741@gmail.com>
2026-01-26 09:05:22 +0200

    Added a Mathematical constants model

---
+++ constants.py
@@ -0,0 +1,18 @@
+"""
+Constants model:
+==================
+Mathematical constants.
+"""
+
+PI = 3.14159265359
+E = 2.71828182846
+GOLDEN_RATIO = 1.61803398875
+
+def show_constants():
+    """
+    Shoes all the Mathematical constants.
+    """
+    print("\n=== Mathematical Constants ===")
+    print(f"π (פאי) = {PI}")
+    print(f"e (אוילר) = {E}")
+    print(f"φ (הזהב יחס) = {GOLDEN_RATIO}")
