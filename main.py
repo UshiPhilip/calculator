6e4d6976e77e8bf51c0637e780163ec9d0c3b7be
Ushi Philip <ushi7670741@gmail.com>
2026-01-26 10:05:30 +0200

    Updated the main & ui file

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from operations import add, subtract, multiply, divide
 from advanced import power, square_root, factorial
 from stats import average, find_max, find_min, median
 from history import add_to_history, show_history, clear_history
+from geometry import circle_area, rectangle_area, triangle_area
+from constants import show_constants
 from ui import get_number, show_menu
 
 print("╔════════════════════════════╗")
@@ -23,68 +25,91 @@ while True:
         if choice == "1":
             result = add(num1, num2)
             print(f"Result: {result}.")
-            add_to_history(f"{num1} + {num2} = {result}")
+            add_to_history(f"{num1} + {num2} =",result)
 
         elif choice == "2":
             result = subtract(num1, num2)
             print(f"Result: {result}.")
-            add_to_history(f"{num1} + {num2} = {result}")
+            add_to_history(f"{num1} + {num2} =",result)
 
         elif choice == "3":
             result = multiply(num1, num2)
             print(f"Result: {result}.")
-            add_to_history(f"{num1} + {num2} = {result}")
+            add_to_history(f"{num1} + {num2} =",result)
 
         elif choice == "4":
             result = divide(num1, num2)
             print(f"Result: {result}.")
-            add_to_history(f"{num1} + {num2} = {result}")
+            add_to_history(f"{num1} + {num2} =",result)
 
         elif choice == "5":
             result = power(num1, num2)
             print(f"Result: {result}.")
-            add_to_history(f"{num1} + {num2} = {result}")
+            add_to_history(f"{num1} + {num2} =",result)
 
     elif choice == "6":
         num = get_number("Enter a number: ")
         result = square_root(num)
         print(f"Result: {result}.")
-        add_to_history(f"{num} ** 0.5 = {result}")
+        add_to_history(f"{num} ** 0.5 =",result)
 
     elif choice == "7":
         num = get_number("Enter a number: ")
         result = factorial(num)
         print(f"Result: {result}")
-        add_to_history(f"{num} != {result}")
+        add_to_history(f"{num} !=",result)
 
     elif choice == "8":
         num = get_number("Enter a number list: ")
         result = average(num)
         print(f"Result: {result}")
-        add_to_history(f"{num}'s average = {result}")
+        add_to_history(f"{num}'s average =",result)
 
     elif choice == "9":
         num = get_number("Enter a numbers list: ")
         result = find_max(num)
         print(f"Result: {result}")
-        add_to_history(f"{num}'s minimum = {result}")
+        add_to_history(f"{num}'s minimum =",result)
 
     elif choice == "10":
         num = get_number("Enter a number list: ")
         result = find_min(num)
         print(f"Result: {result}")
-        add_to_history(f"{num}'s maximum = {result}")
+        add_to_history(f"{num}'s maximum =",result)
 
     elif choice == "11":
         num = get_number("Enter a number list: ")
         result = median(num)
-        print(f"result: {result}")
-        add_to_history(f"{num}'s median = {result}")
+        print(f"Result: {result}")
+        add_to_history(f"{num}'s median =",result)
 
     elif choice == "12":
-        show_history()
+        num = get_number("Enter a radius")
+        result = circle_area(num)
+        print(f"Result: {result}")
+        add_to_history(f"{num}'s area =",result)
 
     elif choice == "13":
+        num1 = get_number("Enter your width: ")
+        num2 = get_number("Enter your height: ")
+        result = rectangle_area(num1, num2)
+        print(f"Result: {result}")
+        add_to_history(f"Width: {num1} & height: {num2}'s area =", result)
+
+    elif choice == "14":
+        num1 = get_number("Enter a base: ")
+        num2 = get_number("Enter a height: ")
+        result = triangle_area(num1, num2)
+        print(f"Result: {result}")
+        add_to_history(f"Base: {num1} & height: {num2}'s area =", result)
+
+    elif choice == "15":
+        show_constants()
+
+    elif choice == "16":
+        show_history()
+
+    elif choice == "17":
         clear_history()
 
     else:
